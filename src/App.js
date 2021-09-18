0f8c7ce04487376e90be2e211632575ef58b9f8c
Mark Katerberg <katerberg@fastmail.fm>
2021-09-18 19:23:58 -0400

    Initial layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="battlefield">
+          <img className="swordfighter" src="https://i.imgur.com/WRXGW2pg.png" alt="Swordfighting" />
+          <img className="monster" src="https://www.psyborg.com.au/wp-content/uploads/2011/11/Illustration-Monster.jpg" alt="Monster" />
+      </div>
+      <div className="distance">3.4m</div>
+      <div className="choices">
+        <Button variant="contained">Attack</Button>
+        <Button variant="outlined">Dodge</Button>
+        <Button variant="outlined">Move</Button>
+      </div>
     </div>
   );
 }
