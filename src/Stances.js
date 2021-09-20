a724ad25afea232f912bd8d9cd1b4f7e5d5083cc
Mark Katerberg <katerberg@fastmail.fm>
2021-09-20 10:39:15 -0500

    Basic guard stances

--- src/Stances.js
+++ src/Stances.js
@@ -1,5 +1,10 @@
+import foolsGuard from './images/guards/fools-guard.png';
+import highGuard from './images/guards/high-guard.png';
+import longpointGuard from './images/guards/longpoint-guard.png';
 import oxGuard from './images/guards/ox-guard.png';
+import plowGuard from './images/guards/plow-guard.png';
 import roofGuard from './images/guards/roof-guard.png';
+import tailGuard from './images/guards/tail-guard.png';
 
 const stances = {
   oxGuard: {
@@ -7,6 +12,21 @@ const stances = {
   },
   roofGuard: {
     image: roofGuard,
+  },
+  foolsGuard: {
+    image: foolsGuard,
+  },
+  highGuard: {
+    image: highGuard,
+  },
+  longpointGuard: {
+    image: longpointGuard,
+  },
+  plowGuard: {
+    image: plowGuard,
+  },
+  tailGuard: {
+    image: tailGuard,
   },
 };
 
